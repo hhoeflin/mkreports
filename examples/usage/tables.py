1b30506426848fb506eefea7299a45c346c7ba52
Holger Hoefling <hhoeflin@gmail.com>
2021-12-19 16:35:38 +0100

    Ensure that it is wrapped in pd.DataFrame to quieten pyright

--- examples/usage/tables.py
+++ examples/usage/tables.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from mkreports import Report, md
 from plotnine.data import mtcars
 
@@ -9,9 +10,31 @@ def use_tables(report: Report) -> None:
     with report.get_page("tables.md", append=False) as p:
         p.add(md.H1("Different ways of handling tables"))
 
+        p.add(
+            """
+            Conveying information with tables is very important for 
+            any type of report. Standard tables in markdown format 
+            can be very useful for this for limited amout of data, but for 
+            larger tables more sophisticated libraries are needed. 
+            """
+        )
         with p.add(md.H2("Markdown tables")):
-            p.add(md.Table(mtcars, index=False))
+            p.add(
+                """
+                Below an example of a regular markdown table. As it is very wide,
+                horizontal scrolling is enabled by default. In addition, the number
+                of rows is limited to 10 as there is no automatic paging available.
+                """
+            )
+            p.add(md.Table(pd.DataFrame(mtcars).head(10), index=False))
 
         with p.add(md.H2("DataTable javascript library")):
+            p.add(
+                """
+                Here the same table, but displayed using the [DataTables](https://datatables.net/)  
+                plugin. With this, we get automatic paging, searching as well as sorting
+                by columns. 
+                """
+            )
             # and as a DataTable
-            p.add(md.DataTable(mtcars))
+            p.add(md.DataTable(pd.DataFrame(mtcars)))
