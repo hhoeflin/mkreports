8a00df87ae80835f0a8dcb1f4a0da3ebac0f44d4
Holger Hoefling <hhoeflin@gmail.com>
2022-04-18 17:23:24 +0000

    correct top-level import

--- mkreports/__init__.py
+++ mkreports/__init__.py
@@ -5,7 +5,8 @@ __version__ = "0.7.1"
 from .config import get_mkreports_dir, set_mkreports_dir
 from .docs import add_pkg_docs
 from .ipython import load_ipython_extension
-from .report import Page, Report
+from .page import Page
+from .report import Report
 from .settings import NavEntry
 from .utils import relative_repo_root
 
